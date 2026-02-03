5f73f6399be968bd56616e1509bad8b832a8dd2a
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2026-02-03 16:42:45 -0500

    updateProductComplete

--- Testing/Models/ProductRepository.cs
+++ Testing/Models/ProductRepository.cs
@@ -5,7 +5,7 @@ using Dapper;
 
 
 namespace Testing.Models
-{
+{ 
     public class ProductRepository : IProductRepository
 
     {
@@ -25,6 +25,12 @@ namespace Testing.Models
         public Product GetProduct(int id)
         {
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+        }
+        
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+                new {name = product.Name, price = product.Price, id = product.ProductID });
         }
     }
 }
